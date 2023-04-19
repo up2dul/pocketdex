d0dbfc81d80dd3e8efb2f563c0b7fb95bdba0013
abdul <malik301002@gmail.com>
2023-04-20 01:58:21 +0700

    chore: tailwind custom colors and idk what to mention it

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      dark: '#094067',
+      light: '#fffffe',
+      gray: '#5f6c7b',
+      primary: '#ef4565',
+      secondary: '#3da9fc',
+    }
   },
   plugins: [],
 };
