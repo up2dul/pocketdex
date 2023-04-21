c0cd04d0c9538b1ec28c7d653f64add855527e35
abdul <malik301002@gmail.com>
2023-04-21 09:17:23 +0700

    feat(comps): Head from next/head

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,21 @@
+import Head from 'next/head';
+
 import Header from './Header';
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <main>
+  <>
+    <Head>
+      <title>Pocketdex</title>
+      <meta name='description' content='Pocketdex' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
     <Header />
 
     <main className='mt-10 px-12 sm:px-24 md:px-32 lg:px-44 xl:px-48'>
       {children}
     </main>
-  </main>
+  </>
 );
 
 export default Layout;
