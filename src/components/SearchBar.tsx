ba2afa36d589f2a110bda7af01b35ca45c776ecd
abdul <malik301002@gmail.com>
2023-04-21 09:53:03 +0700

    feat(comps): add submit handler and `onSubmit` props

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,31 @@
+import { useRef, type FormEvent } from 'react';
+
 import Button from './Button';
 
-const SearchBar = () => (
-  <form className='flex justify-center'>
-    <input
-      type='text'
-      placeholder='Search pokemon by name . . .'
-      autoComplete='off'
-      maxLength={20}
-      className='rounded-l-md bg-gray/10 w-full sm:w-1/2 lg:w-1/3 p-2'
-    />
-    <Button type='submit' className='rounded-l-none'>
-      Go!
-    </Button>
-  </form>
-);
+const SearchBar = ({ onSubmit }: { onSubmit: (q: string) => void }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const query = inputRef.current?.value;
+    onSubmit(query || '');
+  };
+
+  return (
+    <form className='flex justify-center' onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        type='text'
+        placeholder='Search pokemon by name . . .'
+        autoComplete='off'
+        maxLength={20}
+        className='rounded-l-md bg-gray/10 w-full sm:w-1/2 lg:w-1/3 p-2'
+      />
+      <Button type='submit' className='rounded-l-none'>
+        Go!
+      </Button>
+    </form>
+  );
+};
 
 export default SearchBar;
