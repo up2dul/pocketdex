d08de0712a9ce3bc37bce5ec372347c856dc605b
abdul <malik301002@gmail.com>
2023-04-21 11:11:24 +0700

    refactor(comps): Button component to use forwardRef
    This commit refactors the Button component to use forwardRef in order to
    allow the parent component to access the button's DOM node. This change
    enables the Button component to be used in scenarios where direct access
    to the DOM node is required, such as setting focus or getting
    dimensions.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { type PropsWithChildren } from 'react';
+import { forwardRef, type PropsWithChildren } from 'react';
 import { type ClassValue } from 'clsx';
 
 import { cn } from '@/lib/utils';
@@ -12,26 +12,25 @@ type ButtonProps = Partial<
   }>
 >;
 
-const Button = ({
-  type = 'button',
-  isWFull = false,
-  className,
-  onClick,
-  children,
-}: ButtonProps) => (
-  <button
-    type={type}
-    className={cn(
-      'py-2 px-3 bg-primary',
-      'text-light font-medium rounded-md',
-      'hover:bg-primary/80 transition-colors',
-      isWFull && 'w-full',
-      className,
-    )}
-    onClick={onClick}
-  >
-    {children}
-  </button>
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ type = 'button', isWFull = false, className, onClick, children }, ref) => (
+    <button
+      ref={ref}
+      type={type}
+      className={cn(
+        'py-2 px-3 bg-primary',
+        'text-light font-medium rounded-md',
+        'hover:bg-primary/80 transition-colors',
+        isWFull && 'w-full',
+        className,
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  ),
 );
+
+Button.displayName = 'Button';
 
 export default Button;
