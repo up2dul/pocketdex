90c34c22519538467bc9cd5abbbfb5743359eede
abdul <malik301002@gmail.com>
2023-04-21 10:15:07 +0700

    feat(pages): applied `useSearch` and `useInfiniteScroll` hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,49 @@
-import { type NextPage } from 'next';
-import Head from 'next/head';
+import axios from 'axios';
+import { type PokeAPI } from 'pokeapi-types';
 
+import useInfiniteScroll from '@/hooks/use-infinite-scroll';
+import useSearch from '@/hooks/use-search';
+import Button from '@/components/Button';
 import Card from '@/components/Card';
 import SearchBar from '@/components/SearchBar';
 
-const Home: NextPage = () => (
-  <>
-    <Head>
-      <title>Pocketdex</title>
-      <meta name='description' content='Pocketdex' />
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+const Home = ({ pokemonList }: { pokemonList: PokeAPI.NamedAPIResource[] }) => {
+  const { filteredList: searchedList, handleSearch } = useSearch(
+    pokemonList,
+    'name',
+  );
+  const { list: reducedList, increaseLimit } = useInfiniteScroll({
+    initialList: searchedList,
+    initialOffset: 30,
+    limit: 300,
+  });
 
-    <section>
-      <SearchBar />
-    </section>
+  return (
+    <>
+      <section>
+        <SearchBar onSubmit={handleSearch} />
+      </section>
 
-    <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-10'>
-      <Card pokemonName='bulbasaur' url='https://pokeapi.co/api/v2/pokemon/1' />
-      <Card pokemonName='ivysaur' url='https://pokeapi.co/api/v2/pokemon/2' />
-    </section>
-  </>
-);
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-10'>
+        {reducedList?.map(({ name, url }) => (
+          <Card key={`${url}-${name}`} pokemonName={name} url={url} />
+        ))}
+      </section>
+
+      <section className='flex justify-center mt-10'>
+        <Button onClick={increaseLimit}>Load more</Button>
+      </section>
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const response = await axios.get<PokeAPI.NamedAPIResourceList>(
+    'https://pokeapi.co/api/v2/pokemon?offset=0&limit=300',
+  );
+  const pokemonList = response.data.results;
+
+  return { props: { pokemonList } };
+}
 
 export default Home;
